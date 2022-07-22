877e6089991f14570f90f74f55a3d51ab28cfa46
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-07-22 21:46:31 +0530

    Add files via upload

---
+++ Fabonacci Number/src/Fibonacci.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+//Q-> To calculate Fibonacci Series up to n numbers.
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+
+        while (count <= n) {
+            int temp = b;
+            b = b + a;
+            a = temp;
+            count++;
+        }
+
+        System.out.println(b);
+    }
+}
