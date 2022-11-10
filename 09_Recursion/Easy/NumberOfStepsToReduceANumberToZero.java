c31aeb85b52c1dd7b39db869ad89e2bc979188b9
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-11-10 19:57:49 +0530

    Add files via upload

---
+++ 09_Recursion/Easy/NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,65 @@
+/*
+
+https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
+
+Ques_1322------->>>>>>>
+                    Given an integer num, return the number of steps to reduce it to zero.
+
+                    In one step, if the current number is even, you have to divide it by 2, otherwise,
+                    you have to subtract 1 from it.
+
+
+
+Example 1:----->>>>>>>
+                Input: num = 14
+                Output: 6
+                Explanation:----->>>>>>
+                                    Step 1) 14 is even; divide by 2 and obtain 7.
+                                    Step 2) 7 is odd; subtract 1 and obtain 6.
+                                    Step 3) 6 is even; divide by 2 and obtain 3.
+                                    Step 4) 3 is odd; subtract 1 and obtain 2.
+                                    Step 5) 2 is even; divide by 2 and obtain 1.
+                                    Step 6) 1 is odd; subtract 1 and obtain 0.
+
+Example 2:----->>>>>>>
+                Input: num = 8
+                Output: 4
+                Explanation:------>>>>>>
+                                    Step 1) 8 is even; divide by 2 and obtain 4.
+                                    Step 2) 4 is even; divide by 2 and obtain 2.
+                                    Step 3) 2 is even; divide by 2 and obtain 1.
+                                    Step 4) 1 is odd; subtract 1 and obtain 0.
+
+Example 3:------>>>>>
+                Input: num = 123
+                Output: 12
+ */
+public class NumberOfStepsToReduceANumberToZero {
+    public static void main(String[] args) {
+        int num = 123;
+        System.out.println(numberOfSteps(num));
+
+    }
+    public static int numberOfSteps(int num) {
+//        int count = 0;
+//        while (num > 0){
+//            if(num % 2 == 0){
+//                num = num / 2;
+//            }else {
+//                num = num - 1;
+//            }
+//            count ++;
+//        }
+//        return  count;
+        if(num == 0) {
+            return 0;
+        }
+        else if(num % 2 == 0) {
+            return (numberOfSteps(num/2) + 1);
+        }
+        else if(num % 2 == 1) {
+            return (numberOfSteps(num-1) + 1);
+        }
+        return 0;
+    }
+}
