7dc9bdaf2185a810c27fdc57657285cd87b91d8e
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-11-12 19:45:24 +0530

    Add files via upload

---
+++ 08_Patterns/Pattern_X.java
@@ -0,0 +1,25 @@
+/*
+
+                                           *
+                                          * *
+                                         * * *
+                                        * * * *
+                                       * * * * *
+                                      * * * * * *
+                                     * * * * * * *
+
+ */
+public class Pattern_X {
+    public static void main(String[] args) {
+        int n = 7;
+        for (int row = 1; row <= n; row++) {
+            for (int space = 1; space <= n - row; space++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
